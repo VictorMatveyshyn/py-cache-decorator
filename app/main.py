e7c845c4448256bc78fdeec74c935abc949645cf
Victor Matveyshin <vic_m@ukr.net>
2026-03-23 19:24:34 +0200

    first cache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from typing import Callable
 
 
+
+
+
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache1 = {}
+    def wrapper(*args, **kwargs):
+        key = f"{args[0]}:{args[1]}:{args[2]}"
+        answer = cache1.get(key)
+        if answer is None:
+            answer = (args[0] ** args[1] ** args[2]) % (args[0] * args[2])
+            cache1[key] = answer
+        return answer
+    return wrapper
+
+`
