8fa55068e48233da83dae7a9c44323c4aab55e7a
Victor Matveyshin <vic_m@ukr.net>
2026-03-25 16:36:20 +0200

    solved flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache1 = {}
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Any:
 
         key = ":".join([str(num) for num in args])
 
