ad0dfbcae7d96ab3ee6db9d9b59cf54a87cf8f24
Victor Matveyshin <vic_m@ukr.net>
2026-03-25 16:55:08 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,16 +2,16 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache1 = {}
+    cache2 = {}
 
     def wrapper(*args, **kwargs) -> Any:
 
         key = ":".join([str(num) for num in args])
-        answer = cache1.get(key)
+        answer = cache2.get(key)
         if answer is None:
             print("Calculating new result")
             answer = func(*args, **kwargs)
-            cache1[key] = answer
+            cache2[key] = answer
         else:
             print("Getting from cache")
         return answer
